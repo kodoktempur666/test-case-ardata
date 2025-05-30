46c62890cea6a4694ae2bc3f7ab9335cc11a4daa
Danuar Ihza Mahendra <152004732+Danuyaa@users.noreply.github.com>
2025-05-30 16:55:15 +0700

    Delete test-2 directory

--- test-2/server/database/schema.ts
+++
@@ -1,7 +0,0 @@
-import { pgTable, serial, text, varchar } from "drizzle-orm/pg-core";
-
-export const items = pgTable("items", {
-  id: serial("id").primaryKey(),
-  name: varchar("name", { length: 255 }).notNull(),
-  description: text("description"),
-});
--- test-2/server/migrations/0000_colorful_doctor_doom.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE "items" (
-	"id" serial PRIMARY KEY NOT NULL,
-	"name" varchar(255) NOT NULL,
-	"description" text
-);
